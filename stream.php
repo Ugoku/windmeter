6264be97e65dc231a7049f70529aaab2c9c4aab5
Ugoku <github@ugoku.nl>
2015-06-05 15:25:20 +0200

    CORS

--- stream.php
+++ stream.php
@@ -5,9 +5,10 @@
 
 	header('Content-Type: text/event-stream');
 	header('Cache-Control: no-cache');
+	header('Access-Control-Allow-Origin: *'); // Adjust if needed
 
 	while(true) {
-		$file = file_get_contents('http://192.168.2.17/wind.json');
+		$file = file_get_contents('http://192.168.2.17/wind.json'); // Adjust as needed
 		$file = str_replace("\n", '', $file);
 
 		echo 'data: ' . $file . "\n\n";
